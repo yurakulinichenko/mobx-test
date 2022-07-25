f959a584effe87f5fbb581cba65a708260d9bc91
yurakulinichenko <104848480+yurakulinichenko@users.noreply.github.com>
2022-07-25 11:16:26 +0300

    created input

---
+++ my-app/src/shared/BaseUrl.js
@@ -0,0 +1,2 @@
+const baseUrl = "https://youtube.googleapis.com/youtube/v3/search?";
+export default baseUrl;
---
+++ my-app/src/ui/public/Input.js
@@ -0,0 +1,22 @@
+import React from "react";
+import baseUrl from "../../shared/BaseUrl";
+import Key from "../../shared/Key";
+import { makeAutoObservable } from "mobx";
+
+export default class Input extends React.Component {
+  constructor() {
+    makeAutoObservable(this);
+  }
+  state = { videos: [] };
+
+  onVideosInputChange = (e) => {
+    fetch(`${baseUrl}${e.target.value}&key=${Key}`)
+      .then((response) => response.json())
+      .then((json) => this.setState({ videos: json }));
+  };
+
+  render() {
+    console.log(this.state);
+    return <input type="text" onChange={this.onVideosInputChange} />;
+  }
+}
